fc012bb7432c9500ee3d20f629badb406af4f3a2
Matthew Wolf <matthewwolf2015@gmail.com>
2021-02-17 15:57:28 -0500

    Added search bar and table view

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Matthew Wolf on 2/17/21.
+//  Copyright © 2021 Matthew Wolf. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+  var name = ""
+  var artistName = ""
+}
