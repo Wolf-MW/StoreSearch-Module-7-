ba8518db301bd08c1678a0399f206d131a352aa2
Matthew Wolf <matthewwolf2015@gmail.com>
2021-02-23 19:33:29 -0500

    Add Search Class

--- StoreSearch/View Controllers/LandscapeViewController.swift
+++ StoreSearch/View Controllers/LandscapeViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class LandscapeViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
-    var searchResults = [SearchResult]()
+    var search: Search!
     private var firstTime = true
     private var downloads = [URLSessionDownloadTask]()
     
@@ -44,7 +44,7 @@ class LandscapeViewController: UIViewController {
         
         if firstTime {
             firstTime = false
-            tileButtons(searchResults)
+            tileButtons(search.searchResults)
         }
     }
     
